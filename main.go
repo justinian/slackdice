6f8291600026b48d2b62529a4e2bf1548f9f4863
Justin <justin@devjustinian.com>
2015-04-25 14:34:37 -0700

    Added private/public rolling

--- main.go
+++ main.go
@@ -1,22 +1,77 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/justinian/dice"
+	"github.com/kelseyhightower/envconfig"
 )
 
-func rollHandler(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	user := r.PostFormValue("user_name")
-	desc := r.PostFormValue("text")
-	result, _ := dice.Roll(desc)
-	fmt.Fprintf(w, "%s rolled: %v", user, result)
+type SlackMessage struct {
+	Text     string `json:"text"`
+	Username string `json:"username"`
+	Channel  string `json:"channel"`
+	Icon     string `json:"icon_emoji"`
+}
+
+func rollHandler(c Config, private bool) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		desc := r.PostFormValue("text")
+		user := r.PostFormValue("user_name")
+		channel := r.PostFormValue("channel")
+
+		result, err := dice.Roll(desc)
+		if err != nil {
+			fmt.Fprintf(w, "Error: %s", err.Error())
+			return
+		}
+
+		if private {
+			fmt.Fprintf(w, "You rolled: %v", result)
+		} else {
+			m := SlackMessage{
+				Text:     fmt.Sprintf("*%s* rolled `%s` and got:\n_%v_", user, desc, result),
+				Username: "rollbot",
+				Channel:  channel,
+				Icon:     ":d20:",
+			}
+
+			var buf bytes.Buffer
+			enc := json.NewEncoder(&buf)
+			err := enc.Encode(m)
+			if err != nil {
+				fmt.Fprintf(w, "Error: %s", err.Error())
+				return
+			}
+
+			_, err = http.Post(c.SlackUrl, "text/json", &buf)
+			if err != nil {
+				fmt.Fprintf(w, "Error: %s", err.Error())
+			}
+		}
+	}
+}
+
+type Config struct {
+	Listen   string
+	SlackUrl string `envconfig:"slack_url"`
 }
 
 func main() {
-	http.HandleFunc("/roll", rollHandler)
-	log.Fatal(http.ListenAndServe(":8989", nil))
+	c := Config{Listen: ":8000"}
+
+	err := envconfig.Process("slackdice", &c)
+	if err != nil {
+		log.Fatal("Getting config: " + err.Error())
+	}
+
+	http.HandleFunc("/roll", rollHandler(c, false))
+	http.HandleFunc("/roll/private", rollHandler(c, true))
+
+	log.Fatal(http.ListenAndServe(c.Listen, nil))
 }
